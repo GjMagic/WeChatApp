5ea622d708778dd55c8881f0cdcf75ea3b052c85
GjMagic <862635037@qq.com>
2018-05-01 11:08:52 +0800

    完成跳转新闻列表页

--- ReaderMovie/pages/welcome/welcome.js
+++ ReaderMovie/pages/welcome/welcome.js
@@ -1,1 +1,19 @@
-Page({})
+Page({
+  handleTap () {
+    /* // navigateTo是隐藏上一页跳转下一页
+    wx.navigateTo({
+      url: '../post/post'
+    }) */
+
+    // redirectTo是关闭(卸载)上一页跳转下一页
+    wx.redirectTo({
+      url: '../post/post'
+    })
+  },
+  onUnload () {
+    // 卸载页面对应redirectTo
+  },
+  onHide () {
+    // 隐藏页面对应navigateTo
+  }
+})
